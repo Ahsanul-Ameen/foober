3bcd910f1e71f4f8f116c2c4210ddc76b933c08b
Ahsanul-Ameen <ahsanulsabit7@gmail.com>
2021-08-17 23:26:05 +0600

    Add package for Level 4

---
+++ src/Level_4/One/Solution.java
@@ -0,0 +1,14 @@
+package Level_4.One;
+
+public class Solution {
+
+    public static int solution(int[][] map) {
+        // Your code here
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
