fe79670b41a63300c691342158c6ef1842db551f
Ahsanul-Ameen <ahsanulsabit7@gmail.com>
2021-10-07 15:15:02 +0600

    Add package for Level 4 problem 2

---
+++ src/Level_4/Two/Solution.java
@@ -0,0 +1,9 @@
+package Level_4.Two;
+
+public class Solution {
+    public static int[] solution(int[][] times, int times_limit) {
+        // Your code here
+
+        return new int[]{};
+    }
+}
